6dd3b037b4257c2b38d280c7a449d31482e24aa2
Rui Loprd <s21674@alunos.ipca.pt>
2022-10-09 22:58:40 +0100

    Uniforme Cost search algohritm

--- Uninformed Search Algorithms/UniformCostSearch.py
+++ Uninformed Search Algorithms/UniformCostSearch.py
@@ -1,78 +1,58 @@
-# Same as Breadth Search but now the path has costs 
-# The Algorithm
-# 1.  A graph search algorithm tries to find an optimal solution using cost.
-# 2.  Expands nodes with the lowest path cost.
-# 3.  Uses a priority queue for the frontier, sorted by path cost.
-# 4.  Problem definition:
-# Example:
-#       A
-#      / \
-#     B   C
-#    / \   \
-#   D   E-> F
+# Uniform-cost search is an uninformed search algorithm that uses the lowest cumulative cost 
+# to find a path from the source to the destination. Nodes are expanded, starting from the root, 
+# according to the minimum cumulative cost. The uniform-cost search is then implemented using a Priority Queue.
 
-class Vertice:
-    def __init__(self, nome) -> None:
-        self.nome = nome        
-        self.adjacentes = []
+# The Algorithm 
+# 1: Insert the root node into the priority queue
+# 2: Repeat while the queue is not empty:
+#   - Remove the element with the highest priority
+#   - If the removed node is the destination, print total cost and stop the algorithm
+#   - Else, enqueue all the children of the current node to the priority queue, with their cumulative cost from the root as priority
+# https://www.educative.io/answers/what-is-uniform-cost-search
 
-    def add_adjacentes(self, adjacente):
-        self.adjacentes.append(adjacente)
 
-    def show_adjacentes(self):
-        for i in self.adjacentes: # i tem a estrutura do Adjacente
-            print('Vertice: {} - Custo: {}'.format(i.vertice.nome, i.custo))
+graph = {
+    'A': [('B', 5), ('C', 7)],
+    'B': [('D', 8), ('E', 1)],
+    'C': [('D', 9), ('G', 13)],
+    'D': [('G', 4), ('F', 7), ('I', 5)],
+    'E': [],    
+    'F': [],
+    'I': [('G', 14)],
+    'G': [],
+}
 
-class Adjacente:
-    def __init__(self, vertice, custo) -> None:
-        self.vertice = vertice #será to tipo Vertice
-        self.custo = custo
-         
-class Grafo:
-    A = Vertice('A')
-    B = Vertice('B')
-    C = Vertice('C')
-    D = Vertice('D')
-    E = Vertice('E')
-    F = Vertice('F')
-    
-    A.add_adjacentes(Adjacente(B, 5))
-    A.add_adjacentes(Adjacente(C, 7))
-    B.add_adjacentes(Adjacente(D, 2))
-    B.add_adjacentes(Adjacente(F, 6))
-    C.add_adjacentes(Adjacente(F, 10))
-    E.add_adjacentes(Adjacente(F, 3))
-    
 
-# Versão sem estado objectivo que visita todos os nós em pronfudidade
-def ucs(visited, start_node, target_node):
-    
-    visited.append(start_node)
-    queue.append(start_node)
-    path = []
-    count = 0
-    print('Nó visitado:')
-    while queue: # while the queue is not empty
-        
-        atual = queue.pop(0) # guarda o nó atual e remove-o o da fila de nós a visitar
-        
-        print(atual.nome, end = ' ') # end = ' ' coloca tudo na mesma linha
-        for neighbour in atual.adjacentes:
-            if neighbour not in visited:
-                path.append({atual, neighbour.vertice})
-                if neighbour.vertice == target_node:
-                    print ('Caminho encontrado. O custo foi de:', count)
-                    return
-                count += neighbour.custo
-                visited.append(neighbour.vertice)
-                queue.append(neighbour.vertice) # guarda na fila de nós a explorar os seus vizinhos
+def path_cost(path):
+    total_cost = 0
+    for (node, cost) in path:
+        total_cost += cost
+    return total_cost, path[-1][0] # retorna 2 possibilidades de ordenação, primeiro o custo total, caso seja igual a outro caminho, será ordenado por ordem alfabética
 
-     
 
-if __name__ == "__main__":                  
-    visited = [] # List to keep track of visited nodes.
-    queue = [] #list that is used to keep track of nodes currently in the queue
-    grafo = Grafo()
-    grafo.A.show_adjacentes()
-    
-    ucs(visited, grafo.A, grafo.F)
+def ucs(graph, start, goal):
+    queue = [[(start, 0)]]
+    visited = []
+
+    while queue:
+        queue.sort(key = path_cost()) # Orderna a fila pelo caminho mais curto até ao momento
+        path = queue.pop(0) # atribui o primeiro node (menor custo) e remove-o da fila para ser avaliado novamente pelo ultimo node
+        node = path[-1][0] # devolve o nome do node
+
+        if node in visited:
+            continue # não avança volta á itereção com o while
+
+        visited.append(node)
+
+        if node == goal:
+            print(path)
+            return path
+        else:
+            adjacent_nodes = graph.get(node, [])
+            for (node2, cost) in adjacent_nodes:
+                new_path = path.copy() # clona o caminho original
+                new_path.append([node2, cost]) # adiciona o adjacente e o seu custo ao novo caminho
+                queue.append(new_path)
+
+
+ucs(graph, 'A', 'G')
